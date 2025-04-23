f7e8ba8d99542fe028f396ad1881f3b2d31f2e61
tourecodeur <touremacoumba682@gmail.com>
2025-04-23 18:39:42 +0000

    sauvegarde

--- frontend - uchk/src/app/modules/dashboard/dashboard-routing.module.ts
+++ frontend - uchk/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -9,8 +9,6 @@ const routes: Routes = [
     children: [
        { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
        { path: 'candidat', loadChildren: () => import('./candidat/candidat.module').then(m => m.CandidatModule) },
-       { path: 'etudiant', loadChildren: () => import('./etudiant/etudiant.module').then(m => m.EtudiantModule) },
-       { path: 'formateur', loadChildren: () => import('./formateur/formateur.module').then(m => m.FormateurModule) },
        // Redirection par défaut
        { path: '', redirectTo: 'admin', pathMatch: 'full' }
     ]
